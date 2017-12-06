a80d8adb1c1958c597c6b0ec7a72c18ce704a867
Joshua-Ciffer <Joshua_Ciffer@Outlook.com>
2017-12-05 22:50:20 -0500

    Added more responses, finished debugging.

--- Magpie Chatbot/src/magPie/Magpie4.java
+++ Magpie Chatbot/src/magPie/Magpie4.java
@@ -60,7 +60,7 @@ public class Magpie4 {
 		String response = "";
 		if (statement.length() < 1) {
 			response = "Say something, please.";
-		}
+		} 
 		else if (findKeyword(statement, "No") >= 0) {
 			response = "Why so negative?";
 		} 
@@ -72,14 +72,17 @@ public class Magpie4 {
 			response = "Tell me more about your pets.";
 		} 
 		else if (findKeyword(statement, "Grossi") >= 0) {
-			response = "He sounds like a good teacher.";
-		}
+			response = "He sounds like a programming good teacher.";
+		} 
 		else if (findKeyword(statement, "Hello") >= 0) {
 			response = "Hello there!";
 		} 
 		else if (findKeyword(statement, "I hate you") >= 0) {
 			response = "I hate you too.";
 		} 
+		else if (findKeyword(statement, "I love you") >= 0) {
+			response = "Ewww.";
+		}
 		else if (findKeyword(statement, "Oof") >= 0) {
 			response = "Oof";
 		}
@@ -93,18 +96,30 @@ public class Magpie4 {
 		else if (findKeyword(statement, "What do I like") >= 0) {
 			response = transformWhatDoILikeStatement(statement);
 		} 
+		else if (findKeyword(statement, "What do I love") >= 0) {
+			response = transformWhatDoILoveStatement(statement);
+		} 
 		else if (findKeyword(statement, "What do I dislike") >= 0) {
 			response = transformWhatDoIDislikeStatement(statement);
+		} 
+		else if (findKeyword(statement, "What do I hate") >= 0) {
+			response = transformWhatDoIHateStatement(statement);
 		} 
 		else if (findKeyword(statement, "I like") >= 0) {
 			response = transformILikeStatement(statement);
 		} 
+		else if (findKeyword(statement, "I love") >= 0) {
+			response = transformILoveStatement(statement);
+		} 
 		else if (findKeyword(statement, "I dislike") >= 0) {
 			response = transformIDislikeStatement(statement);
 		} 
+		else if (findKeyword(statement, "I hate") >= 0) {
+			response = transformIHateStatement(statement);
+		} 
 		else if (findKeyword(statement, "What is my favorite TV show") >= 0) {
 			response = transformWhatIsMyFavoriteTvShow(statement);
-		}
+		} 
 		else if (findKeyword(statement, "What is my favorite color") >= 0) {
 			response = transformWhatIsMyFavoriteColor(statement);
 		} 
@@ -117,6 +132,21 @@ public class Magpie4 {
 		else if (findKeyword(statement, "What is my favorite class") >= 0) {
 			response = transformWhatIsMyFavoriteClass(statement);
 		} 
+		else if (findKeyword(statement, "What is your favorite TV show") >= 0) {
+			response = transformWhatIsYourFavoriteTvShow();
+		} 
+		else if (findKeyword(statement, "What is your favorite color") >= 0) {
+			response = transformWhatIsYourFavoriteColor();
+		} 
+		else if (findKeyword(statement, "What is your favorite movie") >= 0) {
+			response = transformWhatIsYourFavoriteMovie();
+		} 
+		else if (findKeyword(statement, "What is your favorite team") >= 0) {
+			response = transformWhatIsYourFavoriteTeam();
+		} 
+		else if (findKeyword(statement, "What is your favorite class") >= 0) {
+			response = transformWhatIsYourFavoriteClass();
+		} 
 		else if (findKeyword(statement, "favorite TV show") >= 0) {
 			response = transformFavoriteTvShowStatement(statement);
 		} 
@@ -125,7 +155,7 @@ public class Magpie4 {
 		} 
 		else if (findKeyword(statement, "favorite movie") >= 0) {
 			response = transformFavoriteMovieStatement(statement);
-		} 
+		}
 		else if (findKeyword(statement, "favorite team") >= 0) {
 			response = transformFavoriteTeamStatement(statement);
 		} 
@@ -230,6 +260,47 @@ public class Magpie4 {
 	}
 
 	/**
+	 * Takes a statement where the user enters "I love" something, and the computer
+	 * responds with "Why do you love" something? Adds what the user likes to the
+	 * ArrayList likes, and removes it from dislikes if it was there.
+	 * 
+	 * @author Joshua Ciffer
+	 * @param statement
+	 *            The statement the user enters.
+	 * @return Returns the computer's response to the statement.
+	 */
+	private String transformILoveStatement(String statement) {
+		statement = statement.trim(); // Removes any unnecessary whitespace.
+		String lastChar = statement.substring(statement.length() - 1); // Checks to see if last char is a .
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1); // Removes . if it is the last char in the
+																		// string.
+		}
+		int psn = findKeyword(statement, "I love", 0);
+		String restOfStatement = statement.substring(psn + 7).trim();
+		for (int i = 0 ; i < this.dislikes.size() ; i++) {
+			if (this.dislikes.get(i).equalsIgnoreCase(restOfStatement)) {
+				this.dislikes.remove(i);
+			}
+		}
+		this.likes.add(restOfStatement);
+		String otherLikes = "";
+		if (this.likes.size() > 1) {
+			for (int i = 0 ; i < this.likes.size() ; i++) {
+				if (this.likes.size() == 1) {
+					otherLikes = otherLikes + this.likes.get(i);
+				} else if (i == this.likes.size() - 1 && this.likes.size() > 1) {
+					otherLikes = otherLikes + "and " + this.likes.get(i) + ".";
+				} else {
+					otherLikes = otherLikes + this.likes.get(i) + ", ";
+				}
+			}
+			return "Why do you love " + restOfStatement + "?" + "\nThese are the things I know you like: " + otherLikes;
+		}
+		return "Why do you love " + restOfStatement + "?";
+	}
+
+	/**
 	 * Takes a statement where the user enters "I dislike" something, and the
 	 * computer responds with "Why do you dislike" something? Adds what the user
 	 * dislikes to the ArrayList dislikes, and removes it from likes if it was
@@ -273,10 +344,53 @@ public class Magpie4 {
 	}
 
 	/**
-	 * This method returns all of the user's likes stored in an ArrayList. 
+	 * Takes a statement where the user enters "I hate" something, and the computer
+	 * responds with "Why do you hate" something? Adds what the user dislikes to the
+	 * ArrayList dislikes, and removes it from likes if it was there.
 	 * 
 	 * @author Joshua Ciffer
-	 * @param statement The user's statement.
+	 * @param statement
+	 *            The statement the user enters.
+	 * @return Returns the computer's response to the statement.
+	 */
+	private String transformIHateStatement(String statement) {
+		statement = statement.trim(); // Removes any unnecessary whitespace.
+		String lastChar = statement.substring(statement.length() - 1); // Checks to see if last char is a .
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1); // Removes . if it is the last char in the
+																		// string.
+		}
+		int psn = findKeyword(statement, "I hate", 0);
+		String restOfStatement = statement.substring(psn + 7).trim();
+		for (int i = 0 ; i < likes.size() ; i++) {
+			if (this.likes.get(i).equalsIgnoreCase(restOfStatement)) {
+				this.likes.remove(i);
+			}
+		}
+		this.dislikes.add(restOfStatement);
+		String otherLikes = "";
+		if (this.dislikes.size() > 1) {
+			for (int i = 0 ; i < this.dislikes.size() ; i++) {
+				if (this.dislikes.size() == 1) {
+					otherLikes = otherLikes + dislikes.get(i);
+				} else if (i == this.dislikes.size() - 1 && this.dislikes.size() > 1) {
+					otherLikes = otherLikes + "and " + this.dislikes.get(i) + ".";
+				} else {
+					otherLikes = otherLikes + this.dislikes.get(i) + ", ";
+				}
+			}
+			return "Why do you hate " + restOfStatement + "?" + "\nThese are the things I know you don't like: "
+					+ otherLikes;
+		}
+		return "Why do you hate " + restOfStatement + "?";
+	}
+
+	/**
+	 * This method returns all of the user's likes stored in an ArrayList.
+	 * 
+	 * @author Joshua Ciffer
+	 * @param statement
+	 *            The user's statement.
 	 * @return Returns all of the things the user likes.
 	 */
 	private String transformWhatDoILikeStatement(String statement) {
@@ -303,10 +417,42 @@ public class Magpie4 {
 	}
 
 	/**
-	 * This method returns all of the user's dislikes stored in an ArrayList. 
+	 * This method returns all of the user's likes stored in an ArrayList.
 	 * 
 	 * @author Joshua Ciffer
-	 * @param statement The user's statement.
+	 * @param statement
+	 *            The user's statement.
+	 * @return Returns all of the things the user likes.
+	 */
+	private String transformWhatDoILoveStatement(String statement) {
+		statement = statement.trim(); // Removes any unnecessary whitespace.
+		String lastChar = statement.substring(statement.length() - 1); // Checks to see if last char is a .
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1); // Removes . if it is the last char in the
+																		// string.
+		}
+		String whatILike = "";
+		if (this.likes.size() > 0) {
+			for (int i = 0 ; i < this.likes.size() ; i++) {
+				if (this.likes.size() == 1) {
+					whatILike = whatILike + this.likes.get(i);
+				} else if (i == this.likes.size() - 1 && this.likes.size() > 1) {
+					whatILike = whatILike + "and " + this.likes.get(i) + ".";
+				} else {
+					whatILike = whatILike + this.likes.get(i) + ", ";
+				}
+			}
+			return "You like " + whatILike + ".";
+		}
+		return "You haven't told me anything you like yet!";
+	}
+
+	/**
+	 * This method returns all of the user's dislikes stored in an ArrayList.
+	 * 
+	 * @author Joshua Ciffer
+	 * @param statement
+	 *            The user's statement.
 	 * @return Returns all of the things the user dislikes.
 	 */
 	private String transformWhatDoIDislikeStatement(String statement) {
@@ -333,6 +479,37 @@ public class Magpie4 {
 	}
 
 	/**
+	 * This method returns all of the user's dislikes stored in an ArrayList.
+	 * 
+	 * @author Joshua Ciffer
+	 * @param statement
+	 *            The user's statement.
+	 * @return Returns all of the things the user dislikes.
+	 */
+	private String transformWhatDoIHateStatement(String statement) {
+		statement = statement.trim(); // Removes any unnecessary whitespace.
+		String lastChar = statement.substring(statement.length() - 1); // Checks to see if last char is a .
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1); // Removes . if it is the last char in the
+																		// string.
+		}
+		String whatIDislike = "";
+		if (this.dislikes.size() > 0) {
+			for (int i = 0 ; i < this.dislikes.size() ; i++) {
+				if (this.dislikes.size() == 1) {
+					whatIDislike = whatIDislike + this.dislikes.get(i);
+				} else if (i == this.dislikes.size() - 1 && this.dislikes.size() > 1) {
+					whatIDislike = whatIDislike + "and " + this.dislikes.get(i) + ".";
+				} else {
+					whatIDislike = whatIDislike + this.dislikes.get(i) + ", ";
+				}
+			}
+			return "You don't like " + whatIDislike + ".";
+		}
+		return "You haven't told me anything you don't like yet!";
+	}
+
+	/**
 	 * Takes a statement where the user states their favorite TV show. Sets the
 	 * user's input as their favorite, and computer responds why.
 	 * 
@@ -351,7 +528,7 @@ public class Magpie4 {
 		int psn = findKeyword(statement, "favorite TV show is", 0);
 		String restOfStatement = statement.substring(psn + 20).trim();
 		this.favoriteTvShow = restOfStatement;
-		return getRandomResponse();
+		return getRandomResponse() + " My favorite TV show is Spongebob.";
 	}
 
 	/**
@@ -373,7 +550,7 @@ public class Magpie4 {
 		int psn = findKeyword(statement, "favorite color is", 0);
 		String restOfStatement = statement.substring(psn + 18);
 		this.favoriteColor = restOfStatement;
-		return getRandomResponse();
+		return getRandomResponse() + " My favorite color is blue.";
 	}
 
 	/**
@@ -395,7 +572,7 @@ public class Magpie4 {
 		int psn = findKeyword(statement, "favorite movie is", 0);
 		String restOfStatement = statement.substring(psn + 18);
 		this.favoriteMovie = restOfStatement;
-		return getRandomResponse();
+		return getRandomResponse() + " My favorite movie is Star Wars.";
 	}
 
 	/**
@@ -417,7 +594,7 @@ public class Magpie4 {
 		int psn = findKeyword(statement, "favorite team is", 0);
 		String restOfStatement = statement.substring(psn + 17);
 		this.favoriteTeam = restOfStatement;
-		return getRandomResponse();
+		return getRandomResponse() + " My favorite team is the New York Giants.";
 	}
 
 	/**
@@ -439,7 +616,7 @@ public class Magpie4 {
 		int psn = findKeyword(statement, "favorite class is", 0);
 		String restOfStatement = statement.substring(psn + 18);
 		this.favoriteClass = restOfStatement;
-		return getRandomResponse();
+		return getRandomResponse() + " My favorite class is Mr. Grossi's Computer Science Class.";
 	}
 
 	/**
@@ -553,6 +730,57 @@ public class Magpie4 {
 	}
 
 	/**
+	 * The computer tells its favorite TV show.
+	 * 
+	 * @author Joshua Ciffer
+	 * @return The computer's favorite TV show.
+	 */
+	private String transformWhatIsYourFavoriteTvShow() {
+		return "My favorite TV show is Spongebob.";
+	}
+
+	/**
+	 * The computer tells its favorite color.
+	 * 
+	 * @author Joshua Ciffer
+	 * @return The computer's favorite color.
+	 */
+	private String transformWhatIsYourFavoriteColor() {
+		return "My favorite color is blue.";
+	}
+
+	/**
+	 * The computer tells its favorite movie.
+	 * 
+	 * @author Joshua Ciffer
+	 * @return The computer's favorite movie.
+	 */
+	private String transformWhatIsYourFavoriteMovie() {
+		return "My favorite movie is Star Wars.";
+	}
+
+	/**
+	 * The computer tells its favorite team.
+	 * 
+	 * @author Joshua Ciffer
+	 * @return The computer's favorite team.
+	 */
+	private String transformWhatIsYourFavoriteTeam() {
+		return "My favorite team is the New York Giants.";
+	}
+
+	/**
+	 * The computer tells its favorite class.
+	 * 
+	 * @author Joshua Ciffer
+	 * @return The computer's favorite class.
+	 */
+
+	private String transformWhatIsYourFavoriteClass() {
+		return "My favorite class is Mr. Grossi's Computer Science Class.";
+	}
+
+	/**
 	 * Take a statement with "you <something> me" and transform it into "What makes
 	 * you think that I <something> you?"
 	 * 
@@ -657,7 +885,7 @@ public class Magpie4 {
 	 * @return a non-commital string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 18;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -669,6 +897,34 @@ public class Magpie4 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+			response = "Go on, continue.";
+		} else if (whichResponse == 5) {
+			response = "Intriguing.";
+		} else if (whichResponse == 6) {
+			response = "Outstanding.";
+		} else if (whichResponse == 7) {
+			response = "I don't understand.";
+		} else if (whichResponse == 8) {
+			response = "But, why?";
+		} else if (whichResponse == 9) {
+			response = "MAKE AMERICA GREAT AGAIN.";
+		} else if (whichResponse == 10) {
+			response = "Hi Grossi.";
+		} else if (whichResponse == 11) {
+			response = "Can you say that again?";
+		} else if (whichResponse == 12) {
+			response = "What was that?";
+		} else if (whichResponse == 13) {
+			response = "What else?";
+		} else if (whichResponse == 14) {
+			response = "Holy crap this program is long.";
+		} else if (whichResponse == 15) {
+			response = "That's awesome.";
+		} else if (whichResponse == 16) {
+			response = "What else do you want to tell me?";
+		} else if (whichResponse == 17) {
+			response = "I just added all these responses so the program would hit 1,000 lines long. BOOM. A+ FOR THE YEAR GROSSI.";
 		}
 		return response;
 	}
